5b45d0dd353a32f8a9c3aa0e1d9752f208d27080
Ariel Raudsepp <ariel@Disthene.localdomain>
2021-07-27 22:03:28 -0600

    Set up to bind to random port

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 //! main.rs
+use std::net::TcpListener;
+use tokio;
 use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener: TcpListener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    println!("Attempting to listen on http://127.0.0.1:{}",port);
+    let server = run(listener).expect("Failed to run server");
+    server.await;
+    Ok(())
 }
