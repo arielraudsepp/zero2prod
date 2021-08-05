838b909fa81cc182688ba9f99e446c1393adb920
Ariel Raudsepp <ariel@Disthene.localdomain>
2021-08-05 15:53:57 -0600

    set up first database migration

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -9,7 +9,7 @@ if ! [ -x "$(command -v psql)" ]; then
 fi
 
 # Check if sqlx is installed on OS
-if ! [ -x "$(commanf -v sqlx)" ]; then
+if ! [ -x "$(command -v sqlx)" ]; then
     echo >&2 "Error: 'sqlx' is not installed."
     echo >&2 "Use: "
     echo >&2 "    cargo install --version=0.5.5 sqlx-cli --no-default-features --features postgres"
@@ -26,14 +26,17 @@ DB_NAME="${POSTGRES_DB:=newsletter}"
 # Check if a custom port has been set, otherwise default to '5432'
 DB_PORT="${POSTGRES_PORT:=5432}"
 
-# Launch postgres using Docker
-docker run \
-    -e POSTGRES_USER=${DB_USER} \
-    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-    -e POSTGRES_DB=${DB_NAME} \
-    -p "${DB_PORT}":5432 \
-    -d postgres \
-    postgres -N 1000
+# Launch postgres using Docker, skip if a dockerized Postgres database is already running
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+    docker run \
+        -e POSTGRES_USER=${DB_USER} \
+        -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+        -e POSTGRES_DB=${DB_NAME} \
+        -p "${DB_PORT}":5432 \
+        -d postgres \
+        postgres -N 1000
+fi
 
 # Keep pinging Postgres until it is ready to accept commands
 export PGPASSWORD="${DB_PASSWORD}"
@@ -42,9 +45,12 @@ until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q';
     sleep 1
 done
 
->&2 echo "Postgres is up and running on port ${DB_PORT}!"
+>&2 echo "Postgres is up and running on port ${DB_PORT}! - running migrations now!"
 
 export
 DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
 
 sqlx database create
+sqlx migrate run
+
+>&2 echo "Postgres has been migrated, ready to go!"
