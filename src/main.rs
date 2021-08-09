de137b1113a7f10ac6c74cd7d0e9b40073b555c3
Ariel Raudsepp <ariel@Disthene.localdomain>
2021-08-09 12:16:28 -0600

    set up configuration file

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 //! main.rs
 use std::net::TcpListener;
-use zero2prod::startup::run;
+use zero2prod::{configuration::get_configuration, startup::run};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let listener: TcpListener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
-    println!("Attempting to listen on http://127.0.0.1:{}",port);
-    let server = run(listener).expect("Failed to run server");
-    server.await?;
-    Ok(())
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
