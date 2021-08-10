2392d77c91a275150cf19c464839050c086519df
Ariel Raudsepp <ariel@Disthene.localdomain>
2021-08-10 12:27:13 -0600

    added logger

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 //! main.rs
 use std::net::TcpListener;
-use sqlx::PgPool;
+use sqlx::postgres::PgPool;
 use zero2prod::{configuration::get_configuration, startup::run};
+use env_logger::Env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
