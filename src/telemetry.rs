395e2be5ed7cbfa9b11278756b11ccfa641c11d3
Ariel Raudsepp <ariel@Disthene.localdomain>
2021-08-11 16:06:56 -0600

    allowed toggle of viewing logs for test suite run

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,25 +1,24 @@
 use tracing::subscriber::set_global_default;
+use tracing::Subscriber;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_subscriber::{EnvFilter, Registry, fmt::MakeWriter, layer::SubscriberExt};
 
 pub fn get_subscriber(
     name: String,
-    env_filter: String
-) -> impl Subscriber + Send + Sync {
+    env_filter: String,
+    sink: impl MakeWriter + Send + Sync + 'static,
+) -> impl Subscriber + Sync + Send{
     let env_filter = EnvFilter::try_from_default_env()
-        .unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new(
-        "zero2prod".into(),
-        std::io::stdout
-    );
+        .unwrap_or_else(|_| EnvFilter::new(env_filter));
+    let formatting_layer = BunyanFormattingLayer::new(name, sink);
     Registry::default()
         .with(env_filter)
         .with(JsonStorageLayer)
-        .with(formatting_layer);
+        .with(formatting_layer)
 }
 
-pub fn init_subscriber(subscriber: impl Subcriber + Send + Sync) {
+pub fn init_subscriber(subscriber: impl Subscriber + Sync + Send) {
     LogTracer::init().expect("Failed to set logger");
     set_global_default(subscriber).expect("Failed to set subscriber");
 }
