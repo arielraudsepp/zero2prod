e076fd5c7f54de143891ce7bbe5a0fb4861efbe6
Ariel Raudsepp <ariel@Disthene.localdomain>
2021-08-19 10:16:36 -0600

    added valid email tests

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -23,6 +23,8 @@ impl AsRef<str> for SubscriberEmail {
 
 #[cfg(test)]
 mod tests {
+    use fake::faker::internet::en::SafeEmail;
+    use fake::Fake;
     use super::SubscriberEmail;
     use claim::assert_err;
 
@@ -43,4 +45,19 @@ mod tests {
         let email = "@domain.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
     }
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            let email = SafeEmail().fake_with_rng(g);
+            Self(email)
+        }
+    }
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
+    }
+    dbg!(&valid_email.0);
 }
