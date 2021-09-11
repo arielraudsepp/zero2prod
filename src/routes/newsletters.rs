75c68fd4638fee28b084a354296e88e092e77098
Ariel Raudsepp <ariel@Disthene.local>
2021-09-11 12:29:56 -0700

    created new error type PublishError

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,10 +1,55 @@
 //! src/routes/newsletters.rs
+use actix_web::{HttpResponse, web, ResponseError};
+use sqlx::PgPool;
+use crate::routes::error_chain_fmt;
+use actix_web::http::StatusCode;
 
-use actix_web::{HttpResponse, web};
+#[derive(thiserror::Error)]
+pub enum PublishError {
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
 
+impl std::fmt::Debug for PublishError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>
+    ) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
 
-pub async fn publish_newsletter(_body: web::Json<BodyData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+impl ResponseError for PublishError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            &PublishError::UnexpectedError(_) =>
+                StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
+struct ConfirmedSubscriber {
+    email: String
+}
+
+#[tracing::instrument(name = "Adding a new subscriber",
+skip(pool))]
+async fn get_confirmed_subscribers(
+    pool:&PgPool,
+) -> Result<Vec<ConfirmedSubscriber>, anyhow::Error> {
+    let rows = sqlx::query_as!(
+        ConfirmedSubscriber,
+        r#"SELECT email FROM subscriptions WHERE status = 'confirmed'"#,
+    )
+    .fetch_all(pool)
+    .await?;
+    Ok(rows)
+}
+
+pub async fn publish_newsletter(
+    _body: web::Json<BodyData>,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse, PublishError> {
+    let _subscribers = get_confirmed_subscribers(&pool).await?;
+    Ok(HttpResponse::Ok().finish())
 }
 
 #[derive(serde::Deserialize)]
