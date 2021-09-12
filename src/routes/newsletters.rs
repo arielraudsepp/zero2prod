4c71322644cdf32955a7729248fd41b45fd80191
Ariel Raudsepp <ariel@Disthene.localdomain>
2021-09-12 14:08:18 -0700

    added hashing for password

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -5,6 +5,7 @@ use actix_web::web;
 use actix_web::{HttpResponse, ResponseError};
 use anyhow::Context;
 use sqlx::PgPool;
+use sha3::Digest;
 
 #[derive(thiserror::Error)]
 pub enum PublishError {
@@ -90,10 +91,12 @@ async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
+    let password_hash = sha3::Sha3_256::digest(credentials.password.as_bytes());
+    let password_hash = format!("{:x}", password_hash);
     let user_id: Option<_> = sqlx::query!(
-        r#"SELECT user_id FROM users WHERE username = $1 AND password = $2"#,
+        r#"SELECT user_id FROM users WHERE username = $1 AND password_hash = $2"#,
         credentials.username,
-        credentials.password
+        password_hash
     )
     .fetch_optional(pool)
     .await
