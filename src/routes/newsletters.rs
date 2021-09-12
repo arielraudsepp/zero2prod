d3baacdf84c6dc8d20b9f7fba1ea64c03d89c106
Ariel Raudsepp <ariel@Disthene.localdomain>
2021-09-12 15:10:26 -0700

    converted to PHC string format

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -5,7 +5,7 @@ use actix_web::web;
 use actix_web::{HttpResponse, ResponseError};
 use anyhow::Context;
 use sqlx::PgPool;
-use sha3::Digest;
+use argon2::{Argon2, PasswordHash, PasswordVerifier};
 
 #[derive(thiserror::Error)]
 pub enum PublishError {
@@ -91,22 +91,34 @@ async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
-    let password_hash = sha3::Sha3_256::digest(credentials.password.as_bytes());
-    let password_hash = format!("{:x}", password_hash);
-    let user_id: Option<_> = sqlx::query!(
-        r#"SELECT user_id FROM users WHERE username = $1 AND password_hash = $2"#,
+    let row: Option<_> = sqlx::query!(
+        r#"SELECT user_id, password_hash FROM users WHERE username = $1"#,
         credentials.username,
-        password_hash
     )
     .fetch_optional(pool)
     .await
-    .context("Failed to perform a query to validate auth credentials")
+    .context("Failed to perform a query to retrieve stored credentials")
     .map_err(PublishError::UnexpectedError)?;
 
-    user_id
-        .map(|row| row.user_id)
-        .ok_or_else(|| anyhow::anyhow!("Invalid username or password"))
-        .map_err(PublishError::AuthError)
+    let (expected_password_hash, user_id) = match row {
+        Some(row) => (row.password_hash, row.user_id),
+        None => {
+            return Err(PublishError::AuthError(anyhow::anyhow!(
+                "Unknown username"
+            )));
+        }
+    };
+
+    let expected_password_hash = PasswordHash::new(&expected_password_hash)
+        .context("Failed to parse hash in PHC string format")
+        .map_err(PublishError::UnexpectedError)?;
+
+    Argon2::default()
+        .verify_password(credentials.password.as_bytes(), &expected_password_hash)
+        .context("Invalid password")
+        .map_err(PublishError::AuthError)?;
+
+    Ok(user_id)
 }
 
 #[tracing::instrument(
