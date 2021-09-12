839cbb666cbe2a9e725d18121528e204282cbaad
Ariel Raudsepp <ariel@Disthene.localdomain>
2021-09-12 13:42:00 -0700

    added password verification

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -62,14 +62,76 @@ async fn get_confirmed_subscribers(
     Ok(confirmed_subscribers)
 }
 
+fn basic_authentication(headers: &HeaderMap) ->
+    Result<Credentials, anyhow::Error> {
+        let header_value = headers
+            .get("Authorization")
+            .context("The 'Authorization' header was missing")?
+            .to_str()
+            .context("The 'Authorization' header was not a valid UTF8 string")?;
+        let base64encoded_segment = header_value
+            .strip_prefix("Basic ")
+            .context("The authorization scheme was not 'Basic'")?;
+        let decoded_bytes = base64::decode_config(base64encoded_segment, base64::STANDARD)
+            .context("Failed to base64-decode 'Basic' credentials")?;
+        let decoded_credentials = String::from_utf8(decoded_bytes)
+            .context("The decoded credential string is not valid UTF8")?;
+
+        let mut credentials = decoded_credentials.splitn(2, ':');
+        let username = credentials
+            .next()
+            .ok_or_else(|| anyhow::anyhow!("A username must be provided in 'Basic' auth"))?
+            .to_string();
+        let password = credentials
+            .next()
+            .ok_or_else(|| anyhow::anyhow!("A password must be provided in 'Basic' auth"))?
+            .to_string();
+
+        Ok(Credentials {username, password})
+    }
+
+async fn validate_credentials(
+    credentials: Credentials,
+    pool: &PgPool,
+) -> Result<uuid::Uuid, PublishError> {
+    let user_id: Option<_> = sqlx::query!(
+        r#"SELECT user_id FROM users WHERE username = $1 AND password = $2"#,
+        credentials.username,
+        credentials.password
+    )
+    .fetch_optional(pool)
+    .await
+    .context("Failed to perform a query to validate auth credentials")
+    .map_err(PublishError::UnexpectedError)?;
+
+    user_id
+        .map(|row| row.user_id)
+        .ok_or_else(|| anyhow::anyhow!("Invalid username or password"))
+        .map_err(PublishError::AuthError)
+}
+
+#[tracing::instrument(
+    name = "Publish a newsletter issue",
+    skip(body, pool, email_client, request),
+    fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
+)]
 pub async fn publish_newsletter(
     body: web::Json<BodyData>,
     pool: web::Data<PgPool>,
     email_client: web::Data<EmailClient>,
     request: web::HttpRequest,
 ) -> Result<HttpResponse, PublishError> {
-    let _credentials = basic_authentication(request.headers())
+    let credentials = basic_authentication(request.headers())
         .map_err(PublishError::AuthError)?;
+    tracing::Span::current().record(
+        "username",
+        &tracing::field::display(&credentials.username)
+    );
+    let user_id = validate_credentials(credentials, &pool).await?;
+    tracing::Span::current().record(
+        "user_id",
+        &tracing::field::display(&user_id)
+    );
     let subscribers = get_confirmed_subscribers(&pool).await?;
     for subscriber in subscribers {
         match subscriber {
@@ -114,31 +176,3 @@ struct Credentials {
     username: String,
     password: String,
 }
-
-fn basic_authentication(headers: &HeaderMap) ->
-    Result<Credentials, anyhow::Error> {
-        let header_value = headers
-            .get("Authorization")
-            .context("The 'Authorization' header was missing")?
-            .to_str()
-            .context("The 'Authorization' header was not a valid UTF8 string")?;
-        let base64encoded_segment = header_value
-            .strip_prefix("Basic ")
-            .context("The authorization scheme was not 'Basic'")?;
-        let decoded_bytes = base64::decode_config(base64encoded_segment, base64::STANDARD)
-            .context("Failed to base64-decode 'Basic' credentials")?;
-        let decoded_credentials = String::from_utf8(decoded_bytes)
-            .context("The decoded credential string is not valid UTF8")?;
-
-        let mut credentials = decoded_credentials.splitn(2, ':');
-        let username = credentials
-            .next()
-            .ok_or_else(|| anyhow::anyhow!("A username must be provided in 'Basic' auth"))?
-            .to_string();
-        let password = credentials
-            .next()
-            .ok_or_else(|| anyhow::anyhow!("A password must be provided in 'Basic' auth"))?
-            .to_string();
-
-        Ok(Credentials {username, password})
-    }
